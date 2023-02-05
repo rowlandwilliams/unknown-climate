f5c8229d85da417298d6791415a6fa21081d6afd
rowlandwilliams <rowland.williams.94@gmail.com>
2023-02-05 20:47:20 +0000

    add heading

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,12 +5,32 @@ import { scaleLinear, scaleTime } from "d3-scale";
 import { curveBasis, line } from "d3-shape";
 import { useEffect, useMemo, useState } from "react";
 const padding = 0;
-const ppmExtent = [310, 425];
+const ppmExtent = [312, 423];
 
 export default function Home() {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState<Co2Data[]>([]);
   const { ref, graphWidth, graphHeight } = useResponsiveGraphDims();
+
+  const xScale = useMemo(
+    () =>
+      scaleTime()
+        .domain([new Date(2020, 0, 1), new Date(2020, 11, 31)])
+        .range([padding, graphWidth - padding]),
+    [graphWidth]
+  );
+  const yScale = useMemo(
+    () => scaleLinear().domain(ppmExtent).range([graphHeight, 0]),
+    [graphHeight]
+  );
+  const lineGraphLine = useMemo(
+    () =>
+      line<Day>()
+        .x((d) => xScale(new Date(d.date)))
+        .y((d) => yScale(d.value))
+        .curve(curveBasis),
+    [xScale, yScale]
+  );
 
   useEffect(() => {
     const getData = async () => {
@@ -24,46 +44,26 @@ export default function Home() {
     getData();
   }, []);
 
-  const xScale = useMemo(
-    () =>
-      scaleTime()
-        .domain([new Date(2020, 0, 1), new Date(2020, 11, 31)])
-        .range([padding, graphWidth - padding]),
-    [graphWidth]
-  );
-
-  const yScale = useMemo(
-    () =>
-      scaleLinear()
-        .domain(ppmExtent)
-        .range([graphHeight - padding, padding]),
-    [graphHeight]
-  );
-
-  const lineGraphLine = useMemo(
-    () =>
-      line<Day>()
-        .x((d) => xScale(new Date(d.date)))
-        .y((d) => yScale(d.value))
-        .curve(curveBasis),
-    [xScale, yScale]
-  );
-
   if (loading) return <div />;
   return (
     <>
-      <main className="h-screen bg-white px-8 md:px-32 py-8">
-        <div ref={ref} className="h-full w-full max-w-[600px] mx-auto">
-          <svg height={graphHeight} width={graphWidth}>
-            {data.map(({ year, values }) => (
-              <path
-                d={lineGraphLine(values)}
-                key={year}
-                className="fill-none stroke stroke-gray-600"
-              />
-            ))}
-          </svg>
-        </div>
+      <main className="h-screen bg-zinc-900 px-8 md:px-32 py-24 text-gray-200">
+        <article className="space-y-1 flex flex-col h-full w-full max-w-[600px] mx-auto relative">
+          <section ref={ref} className="flex-grow">
+            <svg height={graphHeight} width={graphWidth}>
+              {data.map(({ year, values }) => (
+                <path
+                  d={lineGraphLine(values) || undefined}
+                  key={year}
+                  className="fill-none stroke stroke-gray-100"
+                />
+              ))}
+            </svg>
+          </section>
+          <h1 className="text-[4rem] whitespace-nowrap text-center leading-10">
+            UNKNOWN CLIMATE
+          </h1>
+        </article>
       </main>
     </>
   );
